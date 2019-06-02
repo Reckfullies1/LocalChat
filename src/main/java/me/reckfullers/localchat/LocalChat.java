737ad15d3843579630272b02faa0c50447d041f8
Devon Gowen <reckfullies@gmail.com>
2019-06-02 11:29:40 -0400

    Initial Commit

---
+++ src/main/java/me/reckfullers/localchat/LocalChat.java
@@ -0,0 +1,19 @@
+package me.reckfullers.localchat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LocalChat extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable()
+    {
+        // Plugin shutdown logic
+    }
+}
